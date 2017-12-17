aae7d83346e6671896873acfc2c79dabad989f00
Jack Wrenn <me@jswrenn.com>
2017-12-17 14:42:01 -0500

    remove mass module

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -3,11 +3,9 @@ mod velocity;
 mod player;
 mod identity;
 mod acceleration;
-mod mass;
 
 pub use self::player::Player;
 pub use self::identity::Identity;
 pub use self::position::Position;
 pub use self::velocity::Velocity;
 pub use self::acceleration::Acceleration;
-pub use self::mass::Mass;
